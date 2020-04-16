1b5a5ab19ca8c25ad4676324c21b0a25dfb1b24c
SupDex <symeton@gmail.com>
2020-04-16 14:29:59 +0300

    add event_types

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,12 @@
 class Event < ApplicationRecord
+  enum event_type: {
+    creator_folder: 'creator_folder',
+    uploader_file: 'uploader_file',
+    unzipper: 'unzipper',
+    zipper: 'zipper',
+    tree_to_file: 'tree_to_file',
+    rename_folder: 'rename_folder',
+    converter_html_to_pdf: 'converter_html_to_pdf',
+    delete_folder: 'delete_folder'
+  }
 end
